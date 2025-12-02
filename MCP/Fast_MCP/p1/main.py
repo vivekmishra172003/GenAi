b6127de3b11aa884e87759a1446a483e91e12540
vivekmishra172003 <vivekmishra172003@gmail.com>
2025-12-02 07:53:10 +0530

    Created a basic server using mcp

--- MCP/Fast_MCP/p1/main.py
+++ MCP/Fast_MCP/p1/main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from p1!")
+import random
+from fastmcp import FastMCP
 
+# Creating server instance
+
+mcp = FastMCP(name="server", port=5000)
+
+@mcp.tool
+def roll_dice(n: int) -> list[int]:
+    return [random.randint(1, 6) for _ in range(n)]
+
+@mcp.tool
+def add(a: int, b: int) -> int:
+    return a + b
 
 if __name__ == "__main__":
-    main()
+    mcp.run()
+
+    
